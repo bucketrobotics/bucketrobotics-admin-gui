b4566d1988600e609e133ecf254184bda9ccdbab
pythonSnake <118955679+pythonSnake5036@users.noreply.github.com>
2023-01-04 21:57:40 +0000

    Start editing features

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -1,10 +1,16 @@
-import { useFirestoreCol } from "../useFirestoreCol";
+import { useFirestoreCol } from "../firestoreHooks";
 import { NewsArticle } from "./NewsArticle";
 import "./news.css";
 import { doc, deleteDoc } from "firebase/firestore";
+import { useState } from "react";
 
 function News(props) {
+    const [editingArticle, setEditingArticle] = useState("");
     const newsArticles = useFirestoreCol(props.db, "news-articles");
+
+    function editArticle(id) {
+        setEditingArticle(id);
+    }
 
     function deleteArticle(id) {
         if (window.confirm(`Are you sure you want to delete the article ${id}?`)) {
@@ -16,8 +22,13 @@ function News(props) {
         <div>
             <h1>News</h1>
             <div className="news-article-container">
-                {newsArticles.map((x, i) => <NewsArticle article={x} key={i} deleteArticle={deleteArticle} />)}
+                {newsArticles.map((x, i) => <NewsArticle article={x} key={i} editArticle={editArticle} deleteArticle={deleteArticle} />)}
             </div>
+            {editingArticle != "" && (
+                <div className="edit-article">
+                    <div className="edit-article-panel"></div>
+                </div>
+            )}
         </div>
     )
 }
